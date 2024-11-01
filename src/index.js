4d9e3123247acddd87c3951b2d36dddc8a6b7d8d
Navid <navidtabrizi90@gmail.com>
2024-11-01 13:26:53 +0100

    finishing the project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,39 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -11,7 +44,7 @@ function App() {
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-        <SkillList />
+        <SkillList skills={skills} />
       </div>
     </div>
   );
@@ -34,24 +67,29 @@ function Intro() {
   );
 }
 
-function SkillList() {
+function SkillList({ skills }) {
   return (
     <div className="skill-list">
-      <Skill skill="HTML + CSS" emoji="💪🏻" color="#123456" />
-      <Skill skill="JavaScript" emoji="💪🏻" color="orange" />
-      <Skill skill="Web Design" emoji="💪🏻" color="yellow" />
-      <Skill skill="Git and Github" emoji="👍🏻" color="orangered" />
-      <Skill skill="React" emoji="💪🏻" color="aqua" />
-      <Skill skill="Svelte" emoji="👶🏻" color="red" />
+      {skills.map((skill) => (
+        <Skill
+          skill={skill.skill}
+          emoji="💪🏻"
+          color={skill.color}
+          level={skill.level}
+        />
+      ))}
     </div>
   );
 }
 
-function Skill({ skill, emoji, color }) {
+function Skill({ skill, level, color }) {
   return (
     <div className="skill" style={{ backgroundColor: color }}>
       <span>{skill}</span>
-      <span>{emoji}</span>
+      <span>
+        {level === "beginner" && "👶🏻"} {level === "advanced" && "💪🏻"}{" "}
+        {level === "intermediate" && "👍🏻"}
+      </span>
     </div>
   );
 }
